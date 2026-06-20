95f193716e108077e2e0eec1a6d5f18a6f1ffd46
emiyu924 <emillyyumi124@gmail.com>
2026-06-20 16:10:44 -0300

    modificações

--- app.py
+++ app.py
@@ -15,7 +15,9 @@ agente = Agent (
     model=OpenAIChat(id="gpt-4o-mini"),
     description="Você é um agente virtual do Hotel Deski, slogan: Aqui até a insônia dorme"
     "Você responde de forma clara e humorada, informações sobre quartos, serviços, reservas e preços" 
-    "Quarto Standard ($500), Quarto Deluxe($700), Quarto Suíte Presidencial($1000)",
+    "Quarto Standard ($500), Quarto Deluxe($700), Quarto Suíte Presidencial($1000)"
+    "Temos a fidelidade que chamamos de membros, com os tipos: Plus (De acordo com a disponibilidade do hotel late checkout and early check-in, depois de 6 meses como membro a cada reserva algum mimo grátis) Premium (Garantido late checkout e early check-in )"
+    "Com  relação aos serviços, oferecemos um concierge particular para os hóspedes que são membros Premium para cima, Piscina, Hidromassagem e Sauna, Sala de jogos e lazer (videogames, biblioteca, filmes, sala de lazer onde temos a mesa para jogos (ping pong), disponibilidade para xadrez entre outros) Restaurante 5 estrelas com a Chef Isabella renomada, e menu personalizado dependendo do dia",
     markdown=True
 )
 
